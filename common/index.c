37cc3dca087002c9090a1143f776c15ca9758aa2
Daniel Moder <daniel.j.moder.18@dartmouth.edu>
2016-08-01 14:12:54 -0400

    Update index.c

--- common/index.c
+++ common/index.c
@@ -7,3 +7,26 @@
  */
 
 /*  write your implementation of the functions exposed in index.h   */
+
+index_t* index_new(numSlots, void (*destructor)(void*))
+{
+    index_t* new = hashtable_new(numSlots, destructor);
+    return new;
+}
+void index_insert(index_t* index, counters_t* ctr)
+{
+    hashtable_insert(index, ctr);
+}
+
+counters_t* index_find(index_t* index, char* keyword)
+{
+    counters_t* ret = hashtable_find(index, keyword);
+    return ret;
+}
+
+void index_delete(index_t* index)
+{
+    hashtable_delete(index);
+}
+
+
