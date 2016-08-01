caad05de928f25b5d0dcf6b115f44f69964706d0
Daniel Moder <daniel.j.moder.18@dartmouth.edu>
2016-08-01 15:02:46 -0400

    Update bag.h

--- lib/bag/bag.h
+++ lib/bag/bag.h
@@ -15,18 +15,4 @@ void* bag_extract(bag_t* bag);
 void bag_delete(bag_t* bag);
 
 
-/* Set value of the counter indicated by key.
- * If the key does not yet exist, create a counter for it
- * and initialize its counter value to 'count'.
- * NULL counters is ignored.
- */
-void counters_set(counters_t *ctrs, int key, int count);
-
-/* Iterate over all counters in the set (in undefined order):
- * call itemfunc for each item, with (arg, key, count).
- */
-void counters_iterate(counters_t *ctrs,
-		      void (*itemfunc)(void *arg, const int key, int count),
-		      void *arg);
-
 #endif // __SET_H
