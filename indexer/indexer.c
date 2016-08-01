9e2916c8a93c732bdc9311b00013900c89af71a0
Daniel Moder <daniel.j.moder.18@dartmouth.edu>
2016-08-01 16:17:31 -0400

    Update indexer.c

--- indexer/indexer.c
+++ indexer/indexer.c
@@ -9,7 +9,21 @@
 
 #include "../common/index.h"
 #include "../common/webpage.h"
+#include "../common/word.h"
 #include "../lib/memory/memory.h"
+
+
+
+void ctrFunc(void *arg, const int key, int count)
+{
+    printf("key: %d, count: %d\n", key, count);
+}
+
+void setFunc(void *arg, const char *key, void *data)
+{
+    printf("iterating through counters: %s\n", key);
+    counters_iterate(data, ctrFunc, NULL);
+}
 
 bool parse(int argc, char* argv[])
 {
@@ -17,34 +31,58 @@ bool parse(int argc, char* argv[])
   return true;
 }
 
-index_t* index_build(char* pageDirectory)
+index_t* index_build(index_t* index, char* pageDirectory)
 {
-  index_t* index = assertp(malloc(sizeof(index)), "index\n");
-  
-  return index;
-
+    WebPage* page;
+    int fileID = 0;
+    
+    while ( (page = webpage_load(pageDirectory, fileID)) != NULL){
+        index_page(index, page, fileID);
+        webpage_delete(page);
+        fileID++;
+    }
 }
 
+void index_page(index_t* index, WebPage* page, int fileID)
+{
+    char* word = NULL;
+    int pos = 0;
+    
+    while ( (pos = GetNextWord(page->html, pos, &word)) != 0){
+        counters_t* ctr = index_find(index, word);
+        
+        if (ctr == NULL){
+            ctr = counters_new();
+            index_insert(index, word, ctr);
+        }
+        counters_add(ctr, fileID);
+        
+        free(word);
+    }
+}
 
 int main(int argc, char* argv[]){
+  int NUMSLOTS = 4;
+  
   parse(argc, argv);
+  
+  
+  
 
   char* pd = argv[1];
   char* fn = argv[2];
 
 
   
-  index_t* index = index_new(count_free);
+  index_t* index = index_new(NUMSLOTS, count_free);
+  index_build(index, "~/cs50/labs/tse/data/output");
   
-
-
-count_free(index);  
-
+  printf("iterating through index\n");
+  hashtable_iterate(index, hashFunc, NULL);
   
+  count_free(index);  
 
   return 0;
-
-
 
 }
 
