dbe8ccf112682daf7f5bd291c4ea2905781410fb
Daniel Moder <daniel.j.moder.18@dartmouth.edu>
2016-08-03 14:08:37 -0400

    Update index.c

--- common/index.c
+++ common/index.c
@@ -9,6 +9,8 @@
 /*  write your implementation of the functions exposed in index.h   */
 
 #include "index.h"
+#include "webpage.h"
+#include "word.h"
 
 index_t* index_new(int numSlots, void (*destructor)(void*))
 {
@@ -30,6 +32,52 @@ counters_t* index_find(index_t* index, char* keyword)
 void index_delete(index_t* index)
 {
     hashtable_delete(index);
+}
+
+
+void index_page(index_t* index, WebPage* page, int fileID)
+{
+    char* word = NULL;
+    int pos = 0;
+    
+    while ( (pos = GetNextWord(page->html, pos, &word)) != 0){
+        if (word == NULL){return;}
+        counters_t* ctr = index_find(index, word);
+        
+        if (ctr == NULL){
+            ctr = counters_new();
+            index_insert(index, word, ctr);
+        }
+        counters_add(ctr, fileID);
+        
+        free(word);
+    }
+}
+
+void index_build(index_t* index, char* pageDirectory)
+{
+
+    WebPage* page;
+    int fileID = 0;
+    
+    while ( (page = webpage_load(pageDirectory, fileID)) != NULL){
+        index_page(index, page, fileID);
+        
+        webpage_delete(page);
+        fileID++;
+    }
+    
+}
+
+void index_save(index_t* index, char* fileName)
+{
+    FILE* fp = NULL;
+    if ( (fp = fopen(fileName, "w")) == NULL){
+        printf("Error: could not open file %s\n", fileName);
+        return;
+    }
+    hashtable_iterate(index, setFunc, fp);
+    fclose(fp);
 }
 
 
