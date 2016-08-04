d84979d2f05fed426447f9dc56b5101190011a9c
Daniel Moder <daniel.j.moder.18@dartmouth.edu>
2016-08-03 21:03:00 -0400

    Update indextest.c

--- indexer/indextest.c
+++ indexer/indextest.c
@@ -24,17 +24,13 @@ char* strdup(char *str)
 
 char* strndup(char* str, int len)
 {
-    printf("in strndup\n");
     char* ret = malloc(len);
     if (! ret) return NULL;
     
     for (int i = 0; i < len; i++){
         ret[i] = str[i];
-        printf(" at index %d / %d\n", i, len);
     }
-    printf("about to set the last char...\n");
     ret[len-1] = '\0';
-    
     return ret;
 }
 
@@ -63,15 +59,13 @@ char* readFile(FILE* file)
 // index_t returned must be free'd by another function
  index_t* index_load(char* indexFileName)
  { 
-    printf("in load\n");
     FILE* fp = NULL;
     if ( (fp = fopen(indexFileName, "r")) == NULL){
         printf("Error: could not open file %s\n", indexFileName);
         return NULL;
     }
     char* indexString = readFile(fp);    
-    printf("read file\n");
-    
+
     int NUMSLOTS = 4;
     index_t* index = index_new(NUMSLOTS, (void (*)(void*))counters_delete); // put the fPointer cast in index_new (it will be that for every index)
     
@@ -87,8 +81,6 @@ char* readFile(FILE* file)
     
     // Ignores numbers! how convenient...
     while ( (pos = GetNextWord(indexString, pos, &word)) != 0){
-        printf("just got word: %s... pos = %d\n", word, pos);
-        printf("indexString[pos] = %c\n", indexString[pos]);
         if (word == NULL){
             free(indexString);
             free(keyword);
@@ -96,29 +88,36 @@ char* readFile(FILE* file)
             return index;
         }
         keyword = strdup(word);
-        printf("keyword = %s\n", keyword);
-        
+
         start = ++pos; // points to first digit of fileID
         while (indexString[pos] != ' '){
             pos++;
-        } printf("out of fileID iteration loop\n");
+        } 
         len = pos - start + 1;
         valP = indexString+start;
         
         fileID = strndup(valP, len);
-        printf("fileID in int form: %d\n", atoi(fileID));
-/*        
+        
+        start = ++pos;
+        while (indexString[pos] != '\n'){
+            pos++;
+        }
+        len = pos - start + 1;
+        valP = indexString+start;
+        
+        count = strndup(valP, len);
+        
         counters_t* ctr = counters_new();
         counters_set(ctr, atoi(fileID), atoi(count));
         index_insert(index, word, ctr);
                 
         free(keyword);
-        free(fileID);
-        free(count);
+//        free(fileID);
+     //   free(count);
                 
         keyword = NULL;
         fileID = NULL;
-        count = NULL;*/
+        count = NULL;
     }
     free(word);
     return NULL; //error -- should not reach this
