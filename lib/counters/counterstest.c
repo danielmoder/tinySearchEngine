0fc282a5d1d5ca71ce0c789a8b66e77d0f795b14
Daniel Moder <daniel.j.moder.18@dartmouth.edu>
2016-08-04 12:44:07 -0400

    Update counterstest.c

--- lib/counters/counterstest.c
+++ lib/counters/counterstest.c
@@ -11,9 +11,7 @@ int main(){
 
   printf("adding to key 4 five times\n");
   counters_add(new, 4);
-  printf("just added once\n");
   counters_add(new, 4);
-  printf("added a second time!\n");
   counters_add(new, 4);
   counters_add(new, 4);
   counters_add(new, 4);
