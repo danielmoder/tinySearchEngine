dc84edf471afd04e9cca05f81cc97e107348990d
Daniel Moder <daniel.j.moder.18@dartmouth.edu>
2016-08-07 12:51:29 -0400

    Update querier.c

--- querier/querier.c
+++ querier/querier.c
@@ -5,6 +5,8 @@ Daniel Moder, Summer 2016
 */
 
 #include <stdio.h>
+#include <string.h>
+
 #include "../common/index.h"
 
 
@@ -28,8 +30,14 @@ int main(int argc, char* argv[])
     char inLine[100];
     // read-in loop
     while ( fgets(inLine, sizeof(inLine), stdin) != NULL){
-        printf("%s\n", inLine);
-    
+        char* word = strtok(inLine, " ");
+        
+        printf("%s\n", word);
+        
+        while ( (word = strtok(NULL, " ")) != NULL){
+            printf("%s\n", word);
+        }
+        
     
         // clean
         // parse        (set of 'and' sequence sets -- implied 'or' b/t sets)
