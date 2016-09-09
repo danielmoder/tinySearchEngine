a1b3591906610cc5b3f27b6ba5f6b852db09d5ea
Daniel Moder <daniel.j.moder.18@dartmouth.edu>
2016-09-09 09:54:13 -0400

    Update crawler.c

--- crawler/crawler.c
+++ crawler/crawler.c
@@ -182,8 +182,9 @@ int main(int argc, char* argv[]){
 
     // Parse arguments
     if (argc != 4){
-        printf("Error: crawler must receive exactly three arguments:\n \
-        1: seedURL\n 2: storage directory\n 3: max depth");
+        printf("Error: crawler must receive exactly three arguments:\n\
+1: seedURL\n 2: storage directory\n 3: max depth");
+
         return 1;
     }
     
